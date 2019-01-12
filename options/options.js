22c6489b99504e0e24188772796f09555e33c2f0
radek-desktop <dolorismachina@gmail.com>
2019-01-12 19:31:12 +0000

    Refactor: Encapsulate in IIFE

--- options/options.js
+++ options/options.js
@@ -1,36 +1,41 @@
-const button = document.querySelector('button')
-button.addEventListener('click', saveToStorage)
-
-
-getFromStorage()
-
-function saveToStorage(e) {
-  const words = processInput()
-
-  browser.storage.local.set({
-    keywords: words
-  })
-  .then(res => console.log(res),
-        err => console.error(err))
-}
-
-
-function getFromStorage() {
-  browser.storage.local.get('keywords')
-  .then(res => {
-      console.log(res)
-      textArea.value = ''
-      res.keywords.forEach(w => textArea.value += w + '\n')
-    },
-        err => console.error(err))
-}
-
-
-function processInput() {
-  const textArea = document.querySelector('textarea')
-  const wordArray = textArea.value.split('\n')
-  const wordArrayTrimmed = wordArray.map(w => w.trim())
-  const arrayFiltered = wordArrayTrimmed.filter(w => w.length > 0)
-
-  return arrayFiltered
-}
+(function() {
+  
+  const button = document.querySelector('button')
+  button.addEventListener('click', saveToStorage)
+  
+  
+  getFromStorage()
+  
+  
+  function saveToStorage(e) {
+    const words = processInput()
+  
+    browser.storage.local.set({
+      keywords: words
+    })
+    .then(res => console.log(res),
+          err => console.error(err))
+  }
+  
+  
+  function getFromStorage() {
+    browser.storage.local.get('keywords')
+    .then(res => {
+        console.log(res)
+        textArea.value = ''
+        res.keywords.forEach(w => textArea.value += w + '\n')
+      },
+          err => console.error(err))
+  }
+  
+  
+  function processInput() {
+    const textArea = document.querySelector('textarea')
+    const wordArray = textArea.value.split('\n')
+    const wordArrayTrimmed = wordArray.map(w => w.trim())
+    const arrayFiltered = wordArrayTrimmed.filter(w => w.length > 0)
+  
+    return arrayFiltered
+  }
+  
+})()
